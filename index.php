d66ba7c8c5a23ea1ff34ddf908e709759e065699
Antoine Wexsteen-Bézu <wexsteenbezuantoine@gmail.com>
2025-12-03 14:16:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <h1>Bienvenu sur Quizzeo !</h1>
     <p>Découvre des Quizz créé par nos utilisateurs, en cliquant <br> sur le bouton ci-dessous :</p>
     <br>
-    <a href="quizzes.php"><button>Voir les Quizz</button></a>
+    <a href="views/quizzes.php"><button>Voir les Quizz</button></a>
 </body>
 </html>
 
