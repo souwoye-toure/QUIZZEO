2d2ee8796a45d710ee26cd6aeaf5fe9b6ed1a666
Antoine Wexsteen-Bézu <wexsteenbezuantoine@gmail.com>
2025-12-03 15:49:37 +0100

    Update stylesheet link path in index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quizzeo</title>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="/../assets/css/style.css">
 </head>
 <body>
     <br>
@@ -13,12 +13,12 @@
     <h1>Bienvenu sur Quizzeo !</h1>
     <p>Découvre des Quizz créé par nos utilisateurs, en cliquant <br> sur le bouton ci-dessous :</p>
     <br>
-    <a href="views/quizzes.php"><button>Voir les Quizz</button></a>
+    <a href="/../views/quizzes.php"><button>Voir les Quizz</button></a>
 </body>
 </html>
 
 <?php
-require_once __DIR__ . '/includes/helpers.php';
-require_once __DIR__ . '/functions/quizzes.php';
+require_once __DIR__ . '/../includes/helpers.php';
+require_once __DIR__ . '/../functions/quizzes.php';
 $quizzes = quizzes_published();
-include __DIR__ . '/views/index.php';
+include __DIR__ . '/../views/index.php';
